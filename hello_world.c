a2773374e9db2b4c1c36f03700907f5da7bea9b3
Tarokh Yaghoubi <tarokhgit@gmail.com>
2024-06-30 01:31:33 +0330

    first commit | Hello world program in C

---
+++ hello_world.c
@@ -0,0 +1,29 @@
+
+// A C program has only one main function
+
+/*
+return_datatype function_name(params) {
+	// code
+	// functionality 
+	
+}
+*/
+
+// stdio -> standard input/output
+
+#include <stdio.h>
+
+
+void foo() {
+	printf("this is foo\n");
+
+}
+
+int main(void) {
+	
+	printf("Hello World, This is KernelKudos \n");
+	foo();
+	return 0;
+}
+
+
